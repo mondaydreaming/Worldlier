85ca7b8c1f851cb5a35ef6dd2dbde9b0d590e1e1
Bishin Ho <bishin.ho@gmail.com>
2015-03-30 10:35:12 +1100

    moving places to tripView from searchView

--- app/assets/javascripts/views/searchView.js
+++ app/assets/javascripts/views/searchView.js
@@ -15,58 +15,22 @@ app.SearchView = Backbone.View.extend({
     event.preventDefault();
     var location = this.$('#location').val();
     var radius = this.$('#radius').val();
-    var placeTag = this.$('#place_tag').val();
+    var val = [];
+    $(':checkbox:checked').each(function(i){
+      val[i] = $(this).val();
+    });
+    var placeTag = val.join('|');
     var numberSights = this.$('#number_sights').val();
     // debugger;
     var saveTrip = new app.Trip ({
       sightsnum: numberSights,
       location: location,
       location_radius: radius,
-      // tag: $("input[type='checkbox']").val();,
+      tag: placeTag
     });
     saveTrip.save().done(function(trip){
       app.trips.add(saveTrip);
-    // Also generate places from google api and save to the database here so that you can render on the next page
-
-    //JSON of Google Places results
-      // var self = this
-      app.trips.fetch().done(function(){
-        var trip = app.trips.findWhere({
-          id: app.trips.last().get('id')
-        });
-        var lat = trip.attributes.latitude;
-        var lng = trip.attributes.longitude;
-        var radius = parseInt(trip.attributes.location_radius)*1000
-        var val = [];
-        $(':checkbox:checked').each(function(i){
-          val[i] = $(this).val();
-        });
-        var tags = val.join('|');
-
-        // var results = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat+','+lng+'&radius='+radius+'&types='+ tags+'&key=AIzaSyCsJcCSDOx5fdOlmWagQZabLeAe6EGxNSI'
-
-        // var searchGPlaces = function() {
-        //   console.log('searching Google Places')
-        //   var GPlacesUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
-
-        //   return $.getJSON(GPlacesUrl), {
-        //     location: lat + ',' +lng,
-        //     radius: radius,
-        //     types: tags,
-        //     key: 'AIzaSyCsJcCSDOx5fdOlmWagQZabLeAe6EGxNSI'
-        //   }
-        // }
-
-        // searchGPlaces().done(function(results){
-        //   console.log(results)
-        //   debugger
-        // });
-
-
-        // console.log(results)   
-        // debugger;
-        app.appRouter.navigate('/trips/' + trip.id, {trigger:true})
-      })
+      app.appRouter.navigate('/trips/' + trip.id, {trigger:true})
     })
   }
 })
@@ -74,3 +38,27 @@ app.SearchView = Backbone.View.extend({
 
 // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&key=AIzaSyCsJcCSDOx5fdOlmWagQZabLeAe6EGxNSI
 
+
+        // var results = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat+','+lng+'&radius='+radius+'&types='+ tags+'&key=AIzaSyCsJcCSDOx5fdOlmWagQZabLeAe6EGxNSI'
+        // debugger;
+
+        // var searchGPlaces = function() {
+        //   console.log('searching Google Places')
+        //   var GPlacesUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+
+        //   return $.ajax(GPlacesUrl, {
+        //     dataType: 'jsonp', // look up JSONP on wikipedia
+        //     data: {
+        //       location: lat + ',' +lng,
+        //       radius: radius,
+        //       types: tags,
+        //       key: 'AIzaSyCsJcCSDOx5fdOlmWagQZabLeAe6EGxNSI'
+        //     }
+        //   });
+        // }
+        // debugger;
+        // searchGPlaces().done(function(results){
+        //   console.log(results)
+        //   debugger;
+        // });
+        // console.log(results)   
--- app/assets/javascripts/views/tripView.js
+++ app/assets/javascripts/views/tripView.js
@@ -15,14 +15,21 @@ app.TripView = Backbone.View.extend({
     var self = this;
 
     app.trips.fetch().done(function(){
-
+      //articulating trip
+      // Trip parameters
       var trip = app.trips.findWhere({
         id: self.model.get('id')
       });
 
       var lat = trip.attributes.latitude;
       var lng = trip.attributes.longitude;
+      var tag = trip.attributes.tag;
+      var sightsnum = trip.attributes.sightsnum;
+      var radius = parseInt(trip.attributes.location_radius)*1000
+      console.log('lat:', lat, 'lng:',lng, 'radius:',radius, 'tags:',tag, 'sights:', sightsnum)
+      debugger;
 
+      // Drawing map for trip
       var mapOptions = {
           center: new google.maps.LatLng(lat, lng),
           zoom: 12,
@@ -30,6 +37,7 @@ app.TripView = Backbone.View.extend({
       };
 
       var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      app.currentMap = map;
 
       var markerOptions = {
         position: new google.maps.LatLng(lat, lng)
@@ -46,6 +54,38 @@ app.TripView = Backbone.View.extend({
       google.maps.event.addListener(marker,'click',function(e){    
         infoWindow.open(map, marker);
       });
+
+      // articulating place
+      // var map;
+      // var service;
+      // var infowindow;
+
+      // function initialize() {
+      //   var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+
+      //   map = new google.maps.Map(document.getElementById('map'), {
+      //       center: pyrmont,
+      //       zoom: 15
+      //     });
+
+      //   var request = {
+      //     location: pyrmont,
+      //     radius: '500',
+      //     types: ['store']
+      //   };
+
+      //   service = new google.maps.places.PlacesService(map);
+      //   service.nearbySearch(request, callback);
+      // }
+
+      // function callback(results, status) {
+      //   if (status == google.maps.places.PlacesServiceStatus.OK) {
+      //     for (var i = 0; i < results.length; i++) {
+      //       var place = results[i];
+      //       createMarker(results[i]);
+      //     }
+      //   }
+      // }
     })   
   }, 
 
