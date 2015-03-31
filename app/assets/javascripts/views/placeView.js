ac0050571e31525a7c6523b65c660a3b129b57dd
Bishin Ho <bishin.ho@gmail.com>
2015-03-31 21:28:41 +1100

    basic styling so ugly i might undo it later

--- app/assets/javascripts/views/placeView.js
+++ app/assets/javascripts/views/placeView.js
@@ -6,5 +6,39 @@ app.PlaceView = Backbone.View.extend({
     console.log('rendering placeView.js');
     var html = $('#placeViewTemplate').html();
     this.$el.html(html)
+// THIS PAGE WONT WORK UNTIL YOU SAVE TO THE DB AND THEN IDENTIFY THE SPECIFIC ID
+    app.places.fetch().done(function(){
+      // ARTICULATING TRIP FOR TIMELINE PURPOSES
+      //WIKI
+      var fetchWikipediaContent= function() {
+        console.log('Fetching wikipedia content');
+        $.ajax({
+          url: 'http://en.wikipedia.org/w/api.php', 
+          data: {
+            action: 'parse',
+            page: place.get('name'),
+            format: 'json',
+            prop: 'text',
+            section: 0
+          },
+            dataType: 'jsonp',
+        }).done(function(result){processWikipediaContent(result)});
+      };
+
+      var processWikipediaContent= function (content) {
+        // Pass in success parameter! If successful, return wiki, if unsuccessful, tell the user to discover and see for themselves - save the place to the database description
+        console.log('Processing wikipedia content')
+        var fetchedRawContent = content.parse.text['*'];
+        var $createElement = $('<div>').html(fetchedRawContent);
+        var $introContent = $createElement.find('p');
+        $('.wiki-container').append($introContent);
+      };
+
+      fetchWikipediaContent();
+
+    })
+
+
+
   }
 })
