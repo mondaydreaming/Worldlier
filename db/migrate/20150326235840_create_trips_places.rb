e3c3dbb118aed1624e0ea6e0104898f5c88d5c74
Bishin Ho <bishin.ho@gmail.com>
2015-04-02 09:06:33 +1100

    fixing pg tables

--- db/migrate/20150326235840_create_trips_places.rb
+++
@@ -1,8 +0,0 @@
-class CreateTripsPlaces < ActiveRecord::Migration
-  def change
-    create_table :trips_places, :id => false do |t|
-      t.integer :trip_id
-      t.integer :place_id
-    end
-  end
-end
--- db/migrate/20150331041831_drop_places_trips.rb
+++
@@ -1,5 +0,0 @@
-class DropPlacesTrips < ActiveRecord::Migration
-  def change
-    drop_table :places_trips
-  end
-end
